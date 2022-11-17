2241009464175abfb8788d2223de980b42b2a7a0
Tim Smart <tim.smart@arisechurch.com>
2022-11-17 22:13:20 +1300

    feat: add common imports to react global

--- packages/react/_src/global.ts
+++ packages/react/_src/global.ts
@@ -7,3 +7,22 @@ import "@react-effect/jotai/global"
  * @tsplus global
  */
 import { createEffectHook } from "@react-effect/react"
+
+/**
+ * @tsplus global
+ */
+import {
+  useState,
+  useEffect,
+  useLayoutEffect,
+  useCallback,
+  useId,
+  useRef,
+  useReducer,
+  useMemo,
+  useContext,
+  createContext,
+  Suspense,
+  ReactNode,
+  Component,
+} from "react"
